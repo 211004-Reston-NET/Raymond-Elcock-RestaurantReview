d0c9e47343d66373b7227faa1c7f8740d0a79dd1
relcock1977 <relcock1977@hotmail.com>
2021-10-07 19:12:16 -0500

    First Commit

---
+++ RRUI/RestaurantMenu.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace RRUI
+{
+    public class RestaurantMenu : IMenu
+    {
+        public void Menu()
+        {
+          Console.WriteLine("Welcome to the Restaurant Menu!");
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("[2] - Checkout");
+            Console.WriteLine("[1] - Buy a Product");
+            Console.WriteLine("[0] - Go to MainMenu");  
+        }
+        public MenuType YourChoice()
+        {
+            string userChoice = Console.ReadLine();
+            switch (userChoice)
+            {
+                case "2" :
+                    return MenuType.RestaurantMenu;
+                case "1" :
+                    return MenuType.RestaurantMenu;
+                case "0" :
+                    return MenuType.RestaurantMenu;
+                default:
+                     Console.WriteLine("Please input a valid response!");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    return MenuType.RestaurantMenu;       
+
+
+                
+                
+            }
+        }
+    }
+}
